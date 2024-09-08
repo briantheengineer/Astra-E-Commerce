d2f1128786e8d79ed097a9ea5373bfe0efc585a5
Brian <brian.castelhano@gmail.com>
2024-09-07 22:08:45 -0300

    completed navbar design, react route implemented

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Landing from './Landing';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Router>
+        <Switch>
+            <Route path="/landing"> <Landing /> </Route>
+        </Switch>
+      </Router>
     </>
   )
 }

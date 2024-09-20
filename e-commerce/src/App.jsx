8218d9a1cdb664f559db20db82f331fe44606894
Brian <brian.castelhano@gmail.com>
2024-09-20 13:52:38 -0300

    Update App.jsx

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer';
 import ProductDetail from './productInfo'
 import Landing from './Landing';
-import React from "react";
+import React, { createContext } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes
 } from "react-router-dom";
+
+const cart = createContext([])
 
 function App() {
   return (

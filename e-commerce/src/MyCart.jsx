e58497ab4b5329b2565aba7060975a5859cd300f
Brian <brian.castelhano@gmail.com>
2024-09-24 13:44:51 -0300

    made cart stored locally

--- e-commerce/src/MyCart.jsx
+++ e-commerce/src/MyCart.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from "react"
 import { CartContext } from './App'
 import "./MyCart.css"
+import { json } from "react-router-dom";
 
 export default function myCart() {
     const { cart, setCart } = useContext(CartContext);
@@ -18,6 +19,18 @@ export default function myCart() {
         setVisibleCart(false)
     )
 }
+    useEffect( () => {
+        const storedCart = localStorage.getItem('cart');
+        if (storedCart) {
+            setCart(JSON.parse(storedCart))
+        }
+    }, []);
+
+    useEffect(() => {
+        if (cart.length > 0) {
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+    }, [cart]);
 
     useEffect(() => { 
         cartFilled()}, [cart])
@@ -34,16 +47,17 @@ export default function myCart() {
 
     return (
         <div className={`${visibleCart ? "visible" : "hidden"} m-5 fixed z-10 max-h-screen right-0 top-1/2 w-1/6 p-2 overflow-clip`}>
-            <div>
-                <button onClick={changeVisibility} className="">Close</button>
-                <ul className=" integralNormal">
+            <div className=" bg-slate-600  p-2">
+                <button onClick={changeVisibility} className=" ml-auto ">Close</button>
+                <ul className="text-center">
                     {cart.length > 0 ? (
                         cart.map((i, index) =>
-                        <li key={index}>{i.name} - {i.price} - {i.quantity} <button onClick={decreaseItem}>hello</button></li>)
+                        <li key={index}> {index + 1}. {i.name} <br></br> ${i.price}  {i.quantity} item <button onClick={decreaseItem}>-</button></li>)
                     ): <li>No Items Added Yet</li>
                     }
                     
                 </ul>
+                <button className="border p-2">View Cart ({cart.length}) </button >
             </div>
         </div>
     )

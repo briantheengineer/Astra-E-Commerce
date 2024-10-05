16757284562b1553f9806b5b4bc2cdb1524808b6
Brian <brian.castelhano@gmail.com>
2024-10-05 16:13:37 -0300

    started styling  buycart

--- e-commerce/src/BuyCart.jsx
+++ e-commerce/src/BuyCart.jsx
@@ -5,15 +5,27 @@ import { Link } from "react-router-dom";
 export default function BuyCart() {
     const {cart, setCart} = useContext(CartContext);
     return (
-        <div className="h-full min-h-72">
+        <div className="h-screen min-h-72">
             <div className="p-5 text-center">
-                {cart.length > 0 ? cart.map(item => <div>
-                <ul>
-                <li>{item.name}</li> <li>{item.price}</li>
-                </ul>
+            <h1 className="integralBold text-3xl mb-2">YOUR CART</h1>
+                {cart.length > 0 ? cart.map(item => 
+                <div>
+                    <ul className="h-fit flex justify-center">
+                        <li className="border content-center justify-center w-2/4 flex">
+                            <div className="h-32 w-32"><img className="w-full h-full object-cover" src={item.img} alt="" /></div>
+                            <div className="flex flex-wrap border w-fit content-evenly"> 
+                                <p className="border w-full h-fit">{item.name} </p>
+                                <div className="flex flex-wrap content-around justify-around w-full">
+                                    <p>Size: {item.size}</p>
+                                    <p>Quantity: {item.quantity}</p>
+                                </div>
+                                <p className="h-fit ml-0">${item.price}</p>
+                                </div>
+                            </li>
+                    </ul>
                 </div>) :
                 <div> 
-                    <h1 className="text-3xl integralBold">NO ITEMS ON YOUR CART</h1>
+                    <h1 className="text-3xl integralBold">IS EMPTY</h1>
                     <Link to='/clothing'><button className="myBtn IntegralRegular text-white text-lg bg-black px-16 py-2 rounded-full mx-10 tracking-widest my-10">Shop Now</button></Link>
                 </div>}
             </div>
--- e-commerce/src/MyCart.jsx
+++ e-commerce/src/MyCart.jsx
@@ -61,17 +61,17 @@ export default function myCart({toggleCart, visibleCart}) {
         <div className={`${visibleCart ? "visible" : "hidden"} myCart fixed z-10 max-h-screen right-0 top-20 w-1/6 overflow-clip text-center rounded-md overflow-y-visible`}>
             <div className="h-fit  p-2">
                 <button onClick={toggleCart} className=" right-3 fixed border p-1 integralNormal">Close</button>
-                <ul className="my-16 border rounded-md">
+                <ul className="my-16 border rounded-md font-semibold">
                     {cart.length > 0 ? (
                         cart.map((i, index) =>
-                        <li key={index} className="border text-sm overflow-hidden">
+                        <li key={index} className="text-sm overflow-hidden">
                             <button className=" text-red-950 fixed p-1 border right-2" onClick={() => removeItem(i)}>X</button>
-                            <div className="flex flex-nowrap border px-2">
+                            <div className="flex flex-nowrap px-2 mt-4">
                                 <div className="h-32  w-fit"><img className="h-full w-full object-contain" src={i.img} alt="" /></div>
                              <div className="self-center">
                                 <h3>{i.name}</h3> <br/>
-                                <div className="flex flex-nowrap justify-evenly"><p>${i.price}</p><p>{i.size}</p></div>
-                               <div className="flex flex-nowrap justify-evenly">Quantity: <button onClick={() => increaseItem(i)}>+</button>{i.quantity}<button onClick={() => decreaseItem(i)}>-</button></div> 
+                                <div className="flex flex-nowrap justify-evenly"><p>{i.size}</p><p>${i.price}</p></div>
+                               <div className="flex flex-nowrap justify-evenly">Qnt: <button className="border rounded-full h-5 w-5" onClick={() => increaseItem(i)}>+</button>{i.quantity}<button className="border h-5 w-5 rounded-full" onClick={() => decreaseItem(i)}>-</button></div> 
                              </div>   
                             </div> 
                         </li>)
@@ -79,8 +79,8 @@ export default function myCart({toggleCart, visibleCart}) {
                     }
                     {}
                 </ul>
-                <div>Subtotal:{totalPrice.toFixed(2)}</div>
-                <Link to="/buycart"><button className="border p-2">View Cart ({cart.length}) </button ></Link>
+                <div className="font-extrabold">Subtotal: {totalPrice.toFixed(2)}</div>
+                <Link to="/buycart"><button className="border p-2 font-bold">View Cart ({cart.length}) </button ></Link>
                 <button onClick={clearCart} className="text-stone-50 integralNormal text-sm underline w-full">CLEAR CART</button>
             </div>
         </div>
